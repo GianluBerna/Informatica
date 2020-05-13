da4b9bfc99b818af3b1f242e5cc0af959c02b6d2
Gianluca <gianluca.bernardi@itiscuneo.eu>
2020-05-13 18:50:57 +0200

    JavaScript

---
+++ JavaScript/ES_03.js
@@ -0,0 +1,50 @@
+
+function car(){
+        document.getElementById("img1").innerHTML = "Car";
+}
+
+function auto(){
+    document.getElementById("img1").innerHTML = "Auto";
+}
+
+function sportscar(){
+    document.getElementById("img2").innerHTML = "Sports car";
+}
+
+function autoSportiva(){
+    document.getElementById("img2").innerHTML = "Auto Sportiva";
+}
+
+function apple(){
+    document.getElementById("img3").innerHTML = "Apple";
+}
+
+function mela(){
+    document.getElementById("img3").innerHTML = "Mela";
+}
+
+function fruitBasket(){
+    document.getElementById("img4").innerHTML = "Fruit Basket";
+}
+
+function cestoFrutta(){
+    document.getElementById("img4").innerHTML = "Cesto di Frutta";
+}
+
+function screwdriver(){
+    document.getElementById("img5").innerHTML = "Screwdriver";
+}
+
+function cacciavite(){
+    document.getElementById("img5").innerHTML = "Cacciavite";
+}
+
+function workTools(){
+    document.getElementById("img6").innerHTML = "Work Tools";
+}
+
+function utensiliLavoro(){
+    document.getElementById("img6").innerHTML = "Utensili da Lavoro";
+}
+
+
